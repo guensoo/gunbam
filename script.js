457e70b43f5537c7a3118a6de8db99d1ecc78dec
ChoiKeen <chbk9195@naver.com>
2025-03-11 18:17:01 +0900

    군밤코딩 3. 11.(화)

---
+++ script.js
@@ -0,0 +1,11 @@
+// ...existing code...
+
+function solution(my_string, k) {
+    return my_string.repeat(k);
+}
+
+// 예제 사용
+let result = solution("hello", 3);
+console.log(result); // "hellohellohello"
+
+// ...existing code...
